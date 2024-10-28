bf249f7451a49921e168b431bcc63076a428db5a
rabia758 <rizwanrabia758@gmail.com>
2024-10-28 23:45:37 +0500

    fist commit

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -6,7 +6,7 @@ export default function About(){
           <Header/>
           <h1 className="bg-grey text-center text-5xl mt-10 underline">About Us</h1>
 
-        <p className="bg-grey text-center text-2xl mt-20">"Hello! This is <span> Rabia Rizwan!" </span> I have completed courses in HTML, CSS, and JavaScript, gaining a solid foundation in web development. With HTML, I can structure content effectively, and with CSS, I create visually appealing designs that enhance user experience. Learning JavaScript has opened up new possibilities for adding interactivity to my projects. I’m excited to apply these skills in future projects, potentially exploring frameworks like React or Vue.js to build dynamic applications. </p>
+        <p className="bg-white text-center text-2xl mt-20">"Hello! This is <span> Rabia Rizwan!" </span> I have completed courses in HTML, CSS, and JavaScript, gaining a solid foundation in web development. With HTML, I can structure content effectively, and with CSS, I create visually appealing designs that enhance user experience. Learning JavaScript has opened up new possibilities for adding interactivity to my projects. I’m excited to apply these skills in future projects, potentially exploring frameworks like React or Vue.js to build dynamic applications. </p>
 
         </div>
     )
--- src/compnents/Footer.tsx
+++ src/compnents/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer(){
     return(
         <>
         <div className="footer"> 
-            <ul className="footer-button">
+            <ul className="footer-btn">
                 <li>Help</li>
                 <li>Privacy POlicy</li>
                 <li>More Info</li>
